c9a2be9f9ff8e96985755232d6a2845e54305cb9
gabrielioliveira19 <54965416+gabrielioliveira19@users.noreply.github.com>
2019-09-19 21:59:09 -0300

    Arquivo fatorial
    Calculo do  Fatorial

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+
+            int i = 1;
+            int n;
+            int   Fat = 1;
+
+
+            Console.WriteLine("Informe um numero:");
+            n = int.Parse(Console.ReadLine());
+            if (n > 0)
+            {
+
+
+                while (i <= n)
+                {
+
+
+                    Fat = Fat * i;
+                    i++;
+                }
+                Console.WriteLine(Fat + "!");
+            }
+            else
+            if( n == 0)
+            {
+                Console.WriteLine(Fat + "!");
+            }
+            else
+                Console.WriteLine("fatorial não permitido");
+        }
+    }
+}
+            
